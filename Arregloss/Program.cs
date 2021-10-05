e9b9152480cb807cfcfc24ec15e9b0cec4c625c8
MarinDM <diegomaarinmontalvo@gmail.com>
2021-10-05 09:12:06 -0500

    Avances Ordenar Arreglo

---
+++ Arregloss/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arregloss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            string letra;
+            arre obj = new arre();
+            do
+            {
+                Console.WriteLine("MENU PRINCIPAL");
+                Console.WriteLine("1.- Inicializar/Borrar");
+                Console.WriteLine("2.- Mostrar");
+                Console.WriteLine("3.- Buscar");
+                Console.WriteLine("4.- Insertar");
+                Console.WriteLine("5.- Eliminar");
+                Console.WriteLine("6.- Modificar");
+                Console.WriteLine("7.- Creditos");
+                Console.WriteLine("8.- Salir");
+                Console.WriteLine("Ingrese un numero para continuar");
+                option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----------------------------");
+                switch (option)
+                {
+                    case 1:
+                        obj.Inicializar();
+                        Console.WriteLine("Arreglo Inicializado");
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case 2:
+                        obj.Ordenar();
+                        obj.Mostrar();
+                        break;
+                    case 3:
+                        Console.WriteLine("Escribe una letra");
+                        letra = Convert.ToString(Console.ReadLine());
+                        obj.Buscar(letra);
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case 4:
+                        Console.WriteLine("Escribe una letra");
+                        letra = Convert.ToString(Console.ReadLine());
+                        obj.Insertar(letra);
+                        Console.WriteLine("----------------------------");
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        Console.WriteLine("INTEGRANTES");
+                        Console.WriteLine("DIEGO MARIN MONTALVO 20170132");
+                        Console.WriteLine("JULIO CESAR MENA AGUILAR 20170157");
+                        Console.WriteLine("JOSE ALEJANDRO PINEDA MAGALLANES 20170023");
+                        break;
+                }
+            } while (option!=8);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arregloss/arre.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arregloss
+{
+    class arre
+    {
+        int i = 0;
+        int max = 30;
+        string[] A = new string[30];
+
+        public int Max { get => max; set => max = value; }
+        public int I { get => i; set => i = value; }
+        public string[] A1 { get => A; set => A = value; }
+
+        public void Inicializar()
+        {
+            string[] A = new string[30];
+        }
+        public string Insertar(string letra)
+        {
+            if (i < 31)
+            {
+                A[i] = letra;
+            }
+            i++;
+            return A[i];
+        }
+        public void Mostrar()
+        {
+            for (int j = 0; j < I; j++)
+            {
+                Console.WriteLine(A[j]);
+            }
+        }
+        public void Buscar(string letra)
+        {
+            for(int a=0;a<I;a++)
+            {
+                if(letra==A[a])
+                {
+                    Console.WriteLine("Letra Encontrada en la poscion " + A[a]);
+                }
+                else
+                {
+                    Console.WriteLine("Letra no encontrada");
+                }
+            }
+        }
+        
+        public void Ordenar()
+        {
+            char a;
+            string let,aux;
+            int pos = 0;
+            for (int j = 97; j < 123; j++)
+            {
+                a = (char)j;
+                for(int k=0; k<I;k++)
+                {
+                    string nom = A[k];
+                    nom = nom.ToLower();
+                    let=nom.Substring(0, 1);
+                    if(let[0]==a)
+                    {
+                        aux = A[pos];
+                        A[pos] = A[k];
+                        A[k] = aux;
+                        pos++;
+                    }
+                }
+            }
+        }
+    }
+}
